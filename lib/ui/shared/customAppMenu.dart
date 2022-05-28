6097cd4d7dc438a49e96027cfa6eb555b88eb284
Bulkatho <bulkatoo@gmail.com>
2022-05-28 16:33:06 -0400

    navegacion basica - navigator 1

--- lib/ui/shared/customAppMenu.dart
+++ lib/ui/shared/customAppMenu.dart
@@ -9,9 +9,20 @@ class CustomAppMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //return _TableDesktopMenu();
+    return LayoutBuilder(
+      builder: ( _ , constraints) {
+        return (constraints.maxWidth > 520)
+          ? _TableDesktopMenu()
+          : _MobileMenu();
+      },
+    );
+  }
+}
 
-    print('Appbar creado');
-
+class _TableDesktopMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
@@ -40,6 +51,43 @@ class CustomAppMenu extends StatelessWidget {
           CustomFlatButtom(
             text: 'Otra pagina',
             //onPressed: () => Navigator.pushNamed(context, '/abc123'),
+            onPressed: () => locator<NavigationService>().navigateTo('/xyz123'),
+            color: Colors.black,
+          ),
+        ],
+      )
+    );
+  }
+}
+
+
+class _MobileMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomFlatButtom(
+            text: 'Contador Statefull',
+            onPressed: () => locator<NavigationService>().navigateTo('/stateful'),
+            color: Colors.black,
+          ),
+
+          const SizedBox(width: 10),
+
+          CustomFlatButtom(
+            text: 'Contador provider',
+            onPressed: () => locator<NavigationService>().navigateTo('/provider'),
+            color: Colors.black,
+          ),
+
+          const SizedBox(width: 10),
+
+          CustomFlatButtom(
+            text: 'Otra pagina',
             onPressed: () => locator<NavigationService>().navigateTo('/xyz123'),
             color: Colors.black,
           ),
