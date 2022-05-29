810cc539e4a44df09a0af8396f3c7f6e351368e5
Bulkatho <bulkatoo@gmail.com>
2022-05-29 01:31:17 -0400

    fin route-fluro

--- lib/ui/views/view_404.dart
+++ lib/ui/views/view_404.dart
@@ -6,21 +6,24 @@ class View404 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text('404', style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
+    return Container(
+      color: Colors.white,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('404', style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
 
-          const SizedBox(height: 10),
+            const SizedBox(height: 10),
 
-          const Text('No se encontró la página', style: TextStyle(fontSize: 20)),
+            const Text('No se encontró la página', style: TextStyle(fontSize: 20)),
 
-          CustomFlatButtom(
-            text: 'Regresar', 
-            onPressed: () => Navigator.pushNamed(context, '/stateful'),
-          )
-        ],
+            CustomFlatButtom(
+              text: 'Regresar', 
+              onPressed: () => Navigator.pushNamed(context, '/stateful'),
+            )
+          ],
+        ),
       ),
     );
   }
